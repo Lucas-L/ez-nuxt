d8966c3c2dcc408be08360e518dc5c0426e8befb
Lucas Lareginie <lareginielucas@hotmail.fr>
2019-10-12 22:10:42 +0200

    Initial commit

---
+++ components/template/componentBase.vue
@@ -0,0 +1,32 @@
+<template>
+    
+</template>
+<script>
+import { mapState,mapMutations } from 'vuex';
+export default {
+    name:'',
+    data: function(){
+        return{
+            test:''
+        }
+    },
+        watch: {
+            test: function (val, oldVal) {
+                console.log('new: %s, old: %s', val, oldVal)
+            },
+        },
+    computed: mapState({
+        // count: state => state.count,
+    }),
+    methods:{
+        
+    },
+    created(){
+        
+        
+    }
+}
+</script>
+<style>
+
+</style>
